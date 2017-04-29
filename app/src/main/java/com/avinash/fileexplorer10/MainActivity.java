d664e3f9b7686dcfa164abe0be377345abeaa0b8
Avinash Sharma <sharma.6197@gmail.com>
2017-04-29 17:23:00 +0530

    Folder navigation established.

--- app/src/main/java/com/avinash/fileexplorer10/MainActivity.java
+++ app/src/main/java/com/avinash/fileexplorer10/MainActivity.java
@@ -1,5 +1,6 @@
 package com.avinash.fileexplorer10;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
@@ -24,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView RV;
 
     Adapter fileAdapter;
+    String FilePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,71 +34,79 @@ public class MainActivity extends AppCompatActivity {
         RV = (RecyclerView) findViewById(rv);
         FileList = new ArrayList<>();
         GenerateFileList();
-        fileAdapter=new Adapter();
+        fileAdapter = new Adapter();
         RV.setAdapter(fileAdapter);
         RV.setLayoutManager(new LinearLayoutManager(this));
-        int s=FileList.size();
- 
-
-
+        int s = FileList.size();
 
 
     }
 
 
-
-
-    void GenerateFileList()
-    {
-       // String extState = Environment.getExternalStorageState();
+    void GenerateFileList() {
+        // String extState = Environment.getExternalStorageState();
       /*  if(!extState.equals(Environment.MEDIA_MOUNTED) || !extState.equals(Environment.MEDIA_MOUNTED_READ_ONLY))
         {
             Log.d("Media not mounted", "Mount SD card");
         }
         else*/
-         String FilePath = Environment.getExternalStorageDirectory().toString();
-            File sd = new File(FilePath);
-            File[] sdDirList = sd.listFiles();
-            for (File aSdDirList : sdDirList) {
-                FileList.add(new String(aSdDirList.getName()));
-                Log.d("GFL", "File");
+         FilePath = Environment.getExternalStorageDirectory().toString();
+        Log.d("GFL",FilePath);
+        File sd = new File(FilePath);
+        File[] sdDirList = sd.listFiles();
+        for (int i = 0; i < sdDirList.length; i++) {
+            FileList.add(new String(sdDirList[i].getName()));
+            Log.d("GFL", "File");
 
         }
     }
 
-    private class VH extends RecyclerView.ViewHolder{
+    private class VH extends RecyclerView.ViewHolder {
         ImageView iv;
         TextView tv;
         CardView CV;
+
         private VH(View itemView) {
             super(itemView);
-            iv= (ImageView) itemView.findViewById(R.id.icon1);
-            tv= (TextView) itemView.findViewById(R.id.tvFname);
-            CV= (CardView) itemView.findViewById(R.id.CV);
+            iv = (ImageView) itemView.findViewById(R.id.icon1);
+            tv = (TextView) itemView.findViewById(R.id.tvFname);
+            CV = (CardView) itemView.findViewById(R.id.CV);
         }
     }
-    public class Adapter extends RecyclerView.Adapter<VH>{
+
+    public class Adapter extends RecyclerView.Adapter<VH> {
 
         @Override
         public VH onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater LI=getLayoutInflater();
-            View v=LI.inflate(R.layout.list_item,parent,false);
+            LayoutInflater LI = getLayoutInflater();
+            View v = LI.inflate(R.layout.list_item, parent, false);
             return new VH(v);
         }
 
         @Override
         public void onBindViewHolder(final VH holder, int position) {
-            String str= FileList.get(position);
+            String str = FileList.get(position);
             holder.tv.setText(str);
-
-
+            holder.CV.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    try {
+                        Intent intent = new Intent(MainActivity.this, Folder_Intent.class);
+                        intent.putExtra("path", FilePath + "/" + holder.tv.getText());
+                        startActivity(intent);
+                    } catch (Exception e) {
+                        Log.e("EXC", e.getMessage());
+                    }
+                }
+            });
         }
 
-        @Override
-        public int getItemCount()
-        {
-            return FileList.size();
-        }
+            @Override
+            public int getItemCount()
+            {
+                return FileList.size();
+            }
+
+
     }
-
 }
